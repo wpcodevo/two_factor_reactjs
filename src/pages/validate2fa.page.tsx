5c3f1f13dca3047f6b6d329f577dbaa4a18deed5
Edem Ziddah <contact@codevoweb.com>
2022-10-04 09:08:47 +0000

    updated

--- src/pages/validate2fa.page.tsx
+++ src/pages/validate2fa.page.tsx
@@ -79,7 +79,7 @@ const Validate2faPage = () => {
   return (
     <section className="bg-ct-blue-600 min-h-screen grid place-items-center">
       <div className="w-full">
-        <h1 className="text-4xl xl:text-6xl text-center font-[600] text-ct-yellow-600 mb-4">
+        <h1 className="text-4xl lg:text-6xl text-center font-[600] text-ct-yellow-600 mb-4">
           Welcome Back
         </h1>
         <h2 className="text-lg text-center mb-4 text-ct-dark-200">
